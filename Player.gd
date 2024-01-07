838dfeea907221a051bb7f162b2f98d705c61360
conradcoding <conradh5656@gmail.com>
2024-01-07 07:16:47 +0000

    Initial commit

---
+++ Player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const PLAYER_SPEED = 50
+
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * PLAYER_SPEED
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	pass
